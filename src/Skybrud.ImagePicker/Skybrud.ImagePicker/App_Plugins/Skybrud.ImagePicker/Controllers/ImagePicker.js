441835db68c87243c401d197b282caad765c4aa4
René Pjengaard Bank <rpjengaard@skybrud.dk>
2016-02-15 23:20:45 +0100

    Initial commit (WIP)

---
+++ src/Skybrud.ImagePicker/Skybrud.ImagePicker/App_Plugins/Skybrud.ImagePicker/Controllers/ImagePicker.js
@@ -0,0 +1,85 @@
+﻿angular.module("umbraco").controller("Skybrud.ImagePicker.Controller", function ($scope, assetsService, dialogService, userService) {
+
+    $scope.cfg = $scope.control.editor.config;
+
+
+    //Image picker related
+    var startNodeId = null;
+    userService.getCurrentUser().then(function (userData) {
+        startNodeId = userData.startMediaId;
+    });
+
+    $scope.addImage = function() {
+        dialogService.mediaPicker({
+            startNodeId: startNodeId,
+            multiPicker: false,
+            onlyImages: true,
+            callback: function(data) {
+                $scope.control.value = data;
+                dialogService.closeAll();
+            }
+        });
+    };
+
+    $scope.removeImage = function() {
+        $scope.control.value = {};
+    };
+
+
+    //LinkPicker related
+    $scope.addLink = function () {
+        dialogService.closeAll();
+        dialogService.linkPicker({
+            callback: function (e) {
+                if (!e.id && !e.url && !confirm('The selected link appears to be empty. Do you want to continue anyways?')) return;
+                $scope.control.value.link = parseUmbracoLink(e);
+                dialogService.closeAll();
+            }
+        });
+    };
+
+    $scope.editLink = function (index) {
+        if (!$scope.control.value.link) {
+            $scope.addLink(index);
+            return;
+        }
+        dialogService.closeAll();
+        if ($scope.control.value.link.mode == 'media') {
+            dialogService.mediaPicker({
+                callback: function (e) {
+                    if (!e.id && !e.url && !confirm('The selected link appears to be empty. Do you want to continue anyways?')) return;
+                    $scope.control.value.link = parseUmbracoLink(e);
+                    dialogService.closeAll();
+                }
+            });
+        } else {
+            dialogService.linkPicker({
+                currentTarget: {
+                    id: $scope.control.value.link.id,
+                    name: $scope.control.value.link.name,
+                    url: $scope.control.value.link.url,
+                    target: $scope.control.value.link.target
+                },
+                callback: function (e) {
+                    if (!e.id && !e.url && !confirm('The selected link appears to be empty. Do you want to continue anyways?')) return;
+                    $scope.control.value.link = parseUmbracoLink(e);
+                    dialogService.closeAll();
+                }
+            });
+        }
+    };
+
+    $scope.removeLink = function () {
+        $scope.control.value.link = null;
+    };
+
+    function parseUmbracoLink(e) {
+        return {
+            id: e.id || 0,
+            name: e.name || '',
+            url: e.url,
+            target: e.target || '_self',
+            mode: (e.id ? (e.isMedia ? 'media' : 'content') : 'url')
+        };
+    }
+});
---
+++ src/Skybrud.ImagePicker/Skybrud.ImagePicker/ImagePickerItem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Skybrud.LinkPicker;
+using System.Web.Mvc;
+using Skybrud.LinkPicker.Extensions.Json;
+using StackExchange.Profiling.Data;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace Skybrud.ImagePicker {
+
+    public class ImagePickerItem {
+        #region Properties
+
+        public IPublishedContent Image { get; private set; }
+
+        public string AltText { get; private set; }
+
+        public MvcHtmlString Description { get; private set; }
+
+        public LinkPickerItem Link { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        internal ImagePickerItem() {}
+
+        #endregion
+
+        #region Static methods
+
+        public static ImagePickerItem Parse(JObject obj) {
+
+            if (obj == null) return null;
+
+            int id = obj.GetInt32("image");  //not sure
+
+            IPublishedContent image = UmbracoContext.Current.MediaCache.GetById(id);
+
+            if (image == null) return null;
+
+            return new ImagePickerItem {
+                Image = image,
+                AltText = obj.GetString("alttext"),
+                Description = obj.GetString("description"),
+                Link = null //TODO:
+            };
+        }
+
+        #endregion
+    }
+}
