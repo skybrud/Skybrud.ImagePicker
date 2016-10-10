ed178f05722785e98b361ab7ca274bf7da93d5a5
Anders Bjerner <abjerner@skybrud.dk>
2016-10-10 15:47:29 +0200

    Rewrote UI and related parts to support multiple images
    - UI now supports both a tiles view and a list/table view
    - UI now supports multiple images
    - The image picker is now isolated in a directive for better re-use
    - Implemented pre-value editor for the property editor
    - Implemented new grid editor
    - etc.

---
+++ src/Skybrud.ImagePicker/App_Plugins/Skybrud.ImagePicker/Services/ImagePickerService.js
@@ -0,0 +1,19 @@
+﻿angular.module('umbraco.services').factory('skybrudImagePickerService', function (dialogService) {
+
+    var service = {
+
+        parseUmbracoLink: function (e) {
+            return {
+                id: e.id || 0,
+                name: e.name || '',
+                url: e.url,
+                target: e.target || '_self',
+                mode: (e.id ? (e.isMedia ? 'media' : 'content') : 'url')
+            };
+        }
+
+    };
+
+    return service;
+
+});
