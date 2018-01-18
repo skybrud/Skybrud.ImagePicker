a749bd9b93c7b1a1054fbcbb61154f31c28b19c2
Anders Bjerner <abjerner@skybrud.dk>
2018-01-18 13:52:28 +0100

    "ImagePickergridConverter" now extends "GridConverterBase"

--- src/Skybrud.ImagePicker/Grid/Converters/ImagePickerGridConverter.cs
+++ src/Skybrud.ImagePicker/Grid/Converters/ImagePickerGridConverter.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Globalization;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using Skybrud.ImagePicker.Grid.Config;
 using Skybrud.ImagePicker.Grid.Values;
 using Skybrud.Umbraco.GridData;
+using Skybrud.Umbraco.GridData.Converters;
 using Skybrud.Umbraco.GridData.Interfaces;
 using Skybrud.Umbraco.GridData.Rendering;
 
@@ -12,15 +11,15 @@ namespace Skybrud.ImagePicker.Grid.Converters {
     /// <summary>
     /// Grid converter for the ImagePicker.
     /// </summary>
-    public class ImagePickerGridConverter : IGridConverter {
+    public class ImagePickerGridConverter : GridConverterBase {
 
         /// <summary>
-        /// Converts the specified <code>token</code> into an instance of <see cref="IGridControlValue"/>.
+        /// Converts the specified <paramref name="token"/> into an instance of <see cref="IGridControlValue"/>.
         /// </summary>
         /// <param name="control">A reference to the parent <see cref="GridControl"/>.</param>
         /// <param name="token">The instance of <see cref="JToken"/> representing the control value.</param>
         /// <param name="value">The converted control value.</param>
-        public bool ConvertControlValue(GridControl control, JToken token, out IGridControlValue value) {
+        public override bool ConvertControlValue(GridControl control, JToken token, out IGridControlValue value) {
             value = null;
             if (IsImagePickerEditor(control.Editor)) {
                 value = GridControlImagePickerValue.Parse(control, token as JObject);
@@ -29,12 +28,12 @@ namespace Skybrud.ImagePicker.Grid.Converters {
         }
 
         /// <summary>
-        /// Converts the specified <code>token</code> into an instance of <see cref="IGridEditorConfig"/>.
+        /// Converts the specified <paramref name="token"/> into an instance of <see cref="IGridEditorConfig"/>.
         /// </summary>
         /// <param name="editor">A reference to the parent <see cref="GridEditor"/>.</param>
         /// <param name="token">The instance of <see cref="JToken"/> representing the editor config.</param>
         /// <param name="config">The converted editor config.</param>
-        public bool ConvertEditorConfig(GridEditor editor, JToken token, out IGridEditorConfig config) {
+        public override bool ConvertEditorConfig(GridEditor editor, JToken token, out IGridEditorConfig config) {
             config = null;
             if (IsImagePickerEditor(editor)) {
                 config = GridEditorImagePickerConfig.Parse(editor, token as JObject);
@@ -43,11 +42,11 @@ namespace Skybrud.ImagePicker.Grid.Converters {
         }
 
         /// <summary>
-        /// Gets an instance <see cref="GridControlWrapper"/> for the specified <code>control</code>.
+        /// Gets an instance <see cref="GridControlWrapper"/> for the specified <paramref name="control"/>.
         /// </summary>
         /// <param name="control">The control to be wrapped.</param>
         /// <param name="wrapper">The wrapper.</param>
-        public bool GetControlWrapper(GridControl control, out GridControlWrapper wrapper) {
+        public override bool GetControlWrapper(GridControl control, out GridControlWrapper wrapper) {
             wrapper = null;
             if (IsImagePickerEditor(control.Editor)) {
                 wrapper = control.GetControlWrapper<GridControlImagePickerValue, GridEditorImagePickerConfig>();
