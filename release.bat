40c0c96685c9b1e818d9220063ec86e7f2e0bc8d
Anders Bjerner <abjerner@skybrud.dk>
2020-11-18 18:44:09 +0100

    Build stuff

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.ImagePicker /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.ImagePicker --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
