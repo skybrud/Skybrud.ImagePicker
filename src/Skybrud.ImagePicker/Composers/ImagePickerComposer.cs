649324f38254e27add5a0b4901238648cd4c580f
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-10-13 13:09:16 +0200

    Added Media tracking for image picker

---
+++ src/Skybrud.ImagePicker/Composers/ImagePickerComposer.cs
@@ -0,0 +1,14 @@
+﻿using Skybrud.ImagePicker.Tracking;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Skybrud.ImagePicker.Composers
+{
+    public class ImagePickerComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.DataValueReferenceFactories().Append<MediaTracking>();
+        }
+    }
+}
---
+++ src/Skybrud.ImagePicker/Tracking/MediaTracking.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Core.Models.Editors;
+using Umbraco.Core.PropertyEditors;
+
+namespace Skybrud.ImagePicker.Tracking
+{
+    public class MediaTracking : IDataValueReferenceFactory, IDataValueReference
+    {
+        public IDataValueReference GetDataValueReference() => this;
+
+        public IEnumerable<UmbracoEntityReference> GetReferences(object value)
+        {
+            var references = new List<UmbracoEntityReference>();
+            if (value != null)
+            {
+                foreach (var image in value.ToString().Split(','))
+                {
+                    var isValid = Udi.TryParse(image, out var udi);
+                    if (isValid)
+                    {
+                        references.Add(new UmbracoEntityReference(udi));
+                    }
+                }
+            }
+            return references;
+        }
+
+        public bool IsForEditor(IDataEditor dataEditor) => dataEditor.Alias.InvariantEquals("Skybrud.ImagePicker.Image");
+    }
+}
